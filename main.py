d923c5226e8d8aa6ffd9e9b8f07cee484f389c0d
phldge1 <paul-herve.louis-dort@universite-paris-saclay.fr>
2025-09-22 13:28:36 +0200

    Add SL and TP calculation

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import os
 
 app = FastAPI()
+
+# Modèle du body JSON
+class SymbolRequest(BaseModel):
+    symbol: str
+    price: float
 
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI on Render"}
 
 @app.post("/predict")
-def predict(symbol: str):
-    return {"signal": "BUY", "confidence": 0.75, "symbol": symbol}
+def predict(request: SymbolRequest):
+    signal = "BUY"
+    confidence = 0.75
+
+    if signal == "BUY":
+        stop_loss = request.price * (1 - 0.005)   # -0.5%
+        take_profit = request.price * (1 + 0.02)  # +2%
+    else:
+        stop_loss = request.price * (1 + 0.005)   # +0.5%
+        take_profit = request.price * (1 - 0.02)  # -2%
+
+    return {
+        "symbol": request.symbol,
+        "price": request.price,
+        "signal": signal,
+        "confidence": confidence,
+        "stop_loss": round(stop_loss, 5),
+        "take_profit": round(take_profit, 5)
+    }
 
 if __name__ == "__main__":
     import uvicorn
